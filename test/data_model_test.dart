330cb0ac3dddb0b37b96ede3c4a9de2f14c7190c
David Kidd <32614126+Davidk20@users.noreply.github.com>
2022-05-24 18:20:16 +0100

    Refactored tests to use a setup method

--- test/data_model_test.dart
+++ test/data_model_test.dart
@@ -2,10 +2,16 @@ import 'package:flutter_core/data_model_puppet.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  late DataModelPuppet one;
+  late DataModelPuppet two;
+
+  setUp((){
+    one = DataModelPuppet.create();
+    two = DataModelPuppet.create();
+  });
+
   test("Test 1 - Two separately initialised instances of DataModel have "
       "different uids", () {
-    DataModelPuppet one = DataModelPuppet.create();
-    DataModelPuppet two = DataModelPuppet.create();
     expect(one.uid != two.uid, true);
   });
 }
