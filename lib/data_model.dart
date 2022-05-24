03efb5c9a1a6e0b1158cf2e87f49b3aa3c9da1b9
David Kidd <32614126+Davidk20@users.noreply.github.com>
2022-05-24 22:25:19 +0100

    Implemented constructor to build instance of DataModel from a JSON mapping

--- lib/data_model.dart
+++ lib/data_model.dart
@@ -10,16 +10,13 @@ import 'package:uuid/uuid.dart';
 /// to allow for the dynamic sorting as implemented within [ListStore].
 abstract class DataModel {
   /// The ID used to uniquely identify the model.
-  String _uid = "";
+  late String uid;
   /// String used to give a name to the model. Name also acts in place of the
   /// title and is used as a way to identify a model.
-  late String _name;
+  late String name;
 
   /// Constructor for a pre-existing object which inherits [DataModel].
-  DataModel(String id, String name) {
-    _uid = id;
-    _name = name;
-  }
+  DataModel(this.uid, this.name);
 
   /// Constructor for a new object inheriting [DataModel]. The [Uuid] class
   /// is used to generate the objects [uid] and is being done in this way so
@@ -28,14 +25,13 @@ abstract class DataModel {
   /// dynamic value, it would become problematic to use the hashcode as the
   /// reference. As such, the UID and hashcode are two different, unique ways
   /// of checking the identity of a model.
-  DataModel.create(String name) {
+  DataModel.create(this.name) {
     var uuid = Uuid();
-    _uid = uuid.v4();
-    _name = name;
+    uid = uuid.v4();
   }
 
-  String get uid => _uid;
-  String get name => _name;
+  /// Constructor instantiating an instance of the object from a JSON object.
+  DataModel.fromJson(Map values);
 
   @override
   bool operator ==(Object other) {
--- lib/data_model_puppet.dart
+++ lib/data_model_puppet.dart
@@ -4,8 +4,23 @@ import 'package:flutter_core/data_model.dart';
 /// class.
 class DataModelPuppet extends DataModel {
 
+  /// Constructor for a pre-existing object which inherits [DataModel].
   DataModelPuppet(String id, String name) : super(id, name);
+
+  /// Constructor for a new object inheriting [DataModel]. The [Uuid] class
+  /// is used to generate the objects [uid] and is being done in this way so
+  /// that the uid for an object is not hashcode dependent. This is important
+  /// as models will be referenced in storage by their uid and if this was a
+  /// dynamic value, it would become problematic to use the hashcode as the
+  /// reference. As such, the UID and hashcode are two different, unique ways
+  /// of checking the identity of a model.
   DataModelPuppet.create(String name) : super.create(name);
+
+  /// Constructor instantiating an instance of the object from a JSON object.
+  DataModelPuppet.fromJson(Map values) : super.fromJson(values) {
+    uid = values["uid"];
+    name = values["name"];
+  }
 
   @override
   bool equals(Object other) {
